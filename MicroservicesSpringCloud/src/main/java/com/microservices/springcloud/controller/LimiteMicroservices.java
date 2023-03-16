6860cf80d7087a5ce2c196945e9454163323c77f
“hariharanpandi” <hariharan.r.pandi@gmail.com>
2023-03-16 21:32:34 +0530

    Microservices

---
+++ MicroservicesSpringCloud/src/main/java/com/microservices/springcloud/controller/LimiteMicroservices.java
@@ -0,0 +1,12 @@
+/*
+ * package com.microservices.springcloud.controller;
+ * 
+ * public class LimiteMicroservices {
+ * 
+ * @Autowired private Configuration configuration;
+ * 
+ * @GetMapping("/limits") public LimitConfiguration
+ * retriveLimitsFromConfigurations() { return new
+ * LimitConfiguration(configuration.getMaximum(), configuration.getMinimum()); }
+ * }
+ */
